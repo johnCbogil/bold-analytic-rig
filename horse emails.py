85f3236ce332cd77af913be498091e0d17be2d53
John Bogil <johncbogil@gmail.com>
2020-10-13 20:52:45 -0400

    rip IDs, not emails

--- horse emails.py
+++ horse emails.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
-#Read in packages
 import pandas as pd
 import numpy as np
 from google.cloud import bigquery
@@ -16,58 +12,25 @@ import datetime
 import re
 warnings.filterwarnings('ignore')
 
-email_list = []       #Initialize empty list to store scraped emails
-
-# In[225]:
-
-def get_table():
+def get_ids():
     #Sling credentials to big queery
     credentials = service_account.Credentials.from_service_account_file(
         'key.json')
 
-    project_id = 'thankful-68f22'
+    client = bigquery.Client(credentials= credentials, project='thankful-68f22')
+    query_job = client.query(
+    """
+    SELECT DISTINCT user_pseudo_id
+    FROM `thankful-68f22.analytics_198686154.events_*`
+    WHERE event_name = 'user_started_trial'
+    AND event_date BETWEEN '20201001' AND '20201013' 
+    """
+    )
 
-    client = bigquery.Client(credentials= credentials,project=project_id)
-
-    #Pull everything from the app's table
-    query_job = client.query("""
-    SELECT * FROM `thankful-68f22.analytics_198686154.events_*` WHERE event_name = 'email_submitted' ORDER BY event_timestamp DESC
-     """)
-
+    #Convert object to df
     results = query_job.result()
-
-    #Pull table into pandas
     df = results.to_dataframe()
     return df
 
-def horse_emails(table_input):
-    
-    ### THIS FUNCTION WILL HORSE EMAILS OUT OF YOUR TABLE ###
-    ### CREATED BY JOSEPH VORBECK 10/13/1994 - MY BDAY ###
-    ### THIS FUNCTION TAKES ONE INPUT:
-       ### A DATAFRAME PULLED FROM BIG QUERY THAT CONTAINS AT LEASTTTTT THE EVENT NAME AND EVEN PARAMS FIELDS ###
-    ### PRETTY SURE THIS IS GUCCI ###
-    
-    
-    # TODO: FIND WAY TO COUNT DYNAMICALLY
-    loop_len = 190 #Obtains length of dataframe used for loop iterations
-    dict_values = list(table_input['event_params'].values) #Obtains values from nested dicts which contain emails
-    email_regex = r'[A-z0-9.]+@+[a-z]+[.]+[a-z]+[a-z]'   #Regex pattern to match emails
-    global email_list
-
-    list_normalized = str(dict_values).replace("'","")  #Replace quotes around emails, just makes it easier
-    reggy = re.findall(email_regex, list_normalized) #Match to email
-    if reggy:                                 
-        email_list += reggy                   #If there is a match put the email in the list
-    else:                                     #If not drop a message
-        print("No email associated")
-
-    return email_list
-
-#Make a copy of the df so you dont have to pull everything from gcp back into memory and waste time type
-table = get_table()
-
-list_of_emails = horse_emails(table)
-
-print(list_of_emails)
-
+ids = get_ids()
+print(ids)
